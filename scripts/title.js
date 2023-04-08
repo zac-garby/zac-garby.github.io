213ee830bed8fdee72b5e6eb820438f4ed947fef
Zac Garby <me@zacgarby.co.uk>
2023-04-08 18:41:19 +0100

    speedup

--- scripts/title.js
+++ scripts/title.js
@@ -11,7 +11,7 @@ var counts = {
 function set(img, letter) {
     if (img.hidden) {
         var index = Math.floor(Math.random() * counts[letter]) + 1
-        img.src = "/images/letters/" + letter + "/" + index + ".png"
+        img.src = "/images/letters/" + letter + "/" + index + ".jpg"
         img.hidden = false
         img.style.transform = "rotate(" + (Math.random() * 8 - 4) + "deg)"
     }
